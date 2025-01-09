06200ae6d45059c589069e1141cd4a3c856601b1
ShreyashD27 <121327080+ShreyashD27@users.noreply.github.com>
2025-01-09 10:57:33 +0530

    Update sendemail.js

--- sendemail.js
+++ sendemail.js
@@ -1,19 +1,17 @@
-var nodejsmailer  = require('nodemailer'); // use mailer nodejs module
+var nodejsmailer  = require('nodemailer'); 
 var mailOptions ={
     from:'shreyashdivekar.numetry@gmail.com',
     to:'shreyashdivekar831@gmail.com',
     subject:"Sending Email to Shreyash",
     text:"Welcome to NodeMailer, It's Working",
     html: '<h1>Welcome</h1><p>That was easy!</p>',
-    // attachments: [
-    //     { filename: 'txt.txt', path: './txt.txt' }
-    //  ]
+   
 }    
 var transporter = nodejsmailer.createTransport({
     service:'gmail',
     auth:{
-        user:'shreyashdivekar.numetry@gmail.com',
-        pass:'nyga kzsw dsya idrg'      // note: always keep password in .env file to keep it hidden
+        user:'yourmail@gmail.com',
+        pass:'yourpassword'     
     }
 });
 
